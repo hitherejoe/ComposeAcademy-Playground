090acabb1db5f6520a46b25deff1841ac857159f
hitherejoe <hitherejoebirch@gmail.com>
2021-03-13 21:34:11 +0000

    Update switch.kt

--- app/src/main/java/co/joebirch/composeplayground/material/switch.kt
+++ app/src/main/java/co/joebirch/composeplayground/material/switch.kt
@@ -10,8 +10,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.ui.tooling.preview.Preview
 import co.joebirch.composeplayground.ComposableLayout
 
 object SwitchView : ComposableLayout {
